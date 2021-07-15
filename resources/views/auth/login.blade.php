c20dbf7f2b49687863eece0d44da4f36bef8c34a
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-07-15 21:40:09 +0900

    Add: Login Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="container text-gray-600 body-font px-5 py-24 mx-auto">
+        <div class="row justify-content-center">
+            <form method="POST" action="{{ route('login') }}"
+                  class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col m-auto w-full mt-10 md:mt-0">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">{{ __('Login ID') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="login_id" type="text" class="form-control @error('login_id') is-invalid @enderror" name="login_id" value="{{ old('login_id') }}" required autofocus>
-
-                                @error('login_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <h2 class="text-gray-900 text-lg font-medium title-font mb-5">ログイン</h2>
+                <div class="relative mb-4">
+                    <label for="login_id" class="leading-7 text-sm text-gray-600">ID</label>
+                    <input type="text" id="login_id" name="login_id"
+                           value="{{ old('login_id') }}" required autofocus
+                           class="@error('login_id') border-themeColor @enderror w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                    @error('login_id')
+                    <span class="text-themeColor">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
-            </div>
+                <div class="relative mb-4">
+                    <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
+                    <input type="password" id="password" name="password"
+                           class="@error('password') border-themeColor @enderror w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                    @error('password')
+                    <span class="text-themeColor">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="block mt-2">
+                    <label class="flex justify-start items-start">
+                        <div
+                            class="bg-white border rounded border-gray-300 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2">
+                            <input type="checkbox" class="opacity-0 absolute" {{ old('remember') ? 'checked' : '' }}>
+                            <svg class="fill-current hidden w-3 h-3 text-themeColor pointer-events-none"
+                                 viewBox="0 0 20 20">
+                                <path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/>
+                            </svg>
+                        </div>
+                        <div class="select-none text-sm text-gray-700 tracking-wide">ログイン情報を記憶する</div>
+                    </label>
+                </div>
+                <button type="submit"
+                    class="text-white bg-themeColor border-0 mt-10 py-2 px-8 focus:outline-none rounded text-lg">
+                    ログイン
+                </button>
+            </form>
         </div>
     </div>
-</div>
 @endsection
