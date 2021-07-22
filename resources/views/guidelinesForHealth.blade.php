346aa848fffbd11701f459f4c2f28664bd0a171c
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-07-22 09:28:40 +0900

    Fix: Guidelines for Health

--- resources/views/guidelinesForHealth.blade.php
+++ resources/views/guidelinesForHealth.blade.php
@@ -3,85 +3,30 @@
 @section('content')
     <div class="mt-3 md:mt-12 px-4 py-4 md:py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div class="max-w-3xl sm:mx-auto">
-            <strong>Health Screening Guideline in the App</strong>
+            <p class="mb-3"><strong>健康診断に利用するデータ</strong></p>
             <p>
-                This page is used to inform visitors regarding my
-                policies with the collection, use, and disclosure of Personal
-                Information if anyone decided to use my Service.
+                本サービスを使用している間、健康診断の提供が必要になる場合があります。
+                このデータは、明らかに発熱をしている、または体調不良である人の入室を拒否するために使用されます。
+                具体的には、体温が37.5度以上であるかどうか、体調が良好であるかどうか、息苦しさなどの症状があるか、だるさや倦怠感を感じることがあるかを尋ねます。
+                体温を37.5度を境に判定する根拠は、厚生労働省の発熱の定義を基にしています。詳しくはこちらをご覧ください。
             </p>
             <p>
-                If you choose to use my Service, then you agree to
-                the collection and use of information in relation to this
-                policy. The Personal Information that I collect is
-                used for providing and improving the Service. I will not use or share your information with
-                anyone except as described in this Privacy Policy.
+                (<a class="text-themeColor" href="https://www.mhlw.go.jp/web/t_doc?dataId=00tb9642&dataType=1&pageNo=1#l000000215">
+                    https://www.mhlw.go.jp/web/t_doc?dataId=00tb9642&dataType=1&pageNo=1 </a>)
             </p>
-            <p><strong>Information Collection and Use</strong></p>
             <p>
-                For a better experience, while using our Service, I
-                may require you to provide us with certain personally
-                identifiable information, including but not limited to Okayama University Student ID, Health Screening.
-                This data is used to deny entry to people who are clearly above normal or unwell.
-                The information that I request will be retained on your device and is not collected by me in any way.
-                This app does not make medical decisions and does not provide medical information.
+                また、息苦しさや倦怠感を判断する根拠は、こちらのページをご覧ください。
             </p>
-            <p><strong>Service Providers</strong></p>
             <p>
-                I may employ third-party companies and
-                individuals due to the following reasons:
+                (<a class="text-themeColor" href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00094.html#h2_free5">
+                    https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00094.html </a>)
             </p>
-            <ul>
-                <li>To facilitate our Service;</li>
-                <li>To provide the Service on our behalf;</li>
-                <li>To perform Service-related services; or</li>
-                <li>To assist us in analyzing how our Service is used.</li>
-            </ul>
             <p>
-                I want to inform users of this Service
-                that these third parties have access to your Personal
-                Information. The reason is to perform the tasks assigned to
-                them on our behalf. However, they are obligated not to
-                disclose or use the information for any other purpose.
+                本アプリ要求する情報はあなたのデバイスに保持され、収集することはありません。
+                また、本アプリは医学的決定を下さず、医学的な情報を提供しません。
+                そのため、このアプリを通じて自身の健康に疑問がある場合は、医師の診断を受けて、その指示に従ってください。
             </p>
-            <p><strong>Security</strong></p>
-            <p>
-                I value your trust in providing us your
-                Personal Information, thus we are striving to use commercially
-                acceptable means of protecting it. But remember that no method
-                of transmission over the internet, or method of electronic
-                storage is 100% secure and reliable, and I cannot
-                guarantee its absolute security.
-            </p>
-            <p><strong>Children’s Privacy</strong></p>
-            <p>
-                These Services do not address anyone under the age of 13.
-                I do not knowingly collect personally
-                identifiable information from children under 13 years of age. In the case
-                I discover that a child under 13 has provided
-                me with personal information, I immediately
-                delete this from our servers. If you are a parent or guardian
-                and you are aware that your child has provided us with
-                personal information, please contact me so that
-                I will be able to do necessary actions.
-            </p>
-            <p><strong>Changes to This Guideline</strong></p>
-            <p>
-                I may update this Guideline from
-                time to time. Thus, you are advised to review this page
-                periodically for any changes. I will
-                notify you of any changes by posting the new Guideline on
-                this page.
-            </p>
-            <p>This Guideline is effective as of 2021-07-12</p>
-            <p><strong>Contact Us</strong></p>
-            <p>
-                If you have any questions or suggestions about my
-                Guideline, do not hesitate to contact me at developer.ikep@gmail.com.
-            </p>
-            <p>This Guideline page was created at <a href="https://privacypolicytemplate.net" target="_blank"
-                                                          rel="noopener noreferrer">privacypolicytemplate.net </a>and
-                modified/generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank"
-                                         rel="noopener noreferrer">App Privacy Policy Generator</a></p>
+            <p class="mt-4">（このガイドラインは、2021年7月22日に制定されました）</p>
         </div>
     </div>
 @endsection
