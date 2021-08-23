b04fad04a5d23be828aa8aabcd1c909895f82071
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-08-24 01:35:15 +0900

    Change: Significantly Changed Privacy Policy

--- resources/views/privacyPolicy.blade.php
+++ resources/views/privacyPolicy.blade.php
@@ -5,190 +5,50 @@
     <div
         class="px-4 py-4 md:py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
         <div class="sm:mx-auto">
-            <p class="mb-3 text-lg"><strong>Privacy Policy</strong></p>
-            <p class="text-sm">
-                Kaito Ikeda built the cacao app as
-                an Open Source app. This SERVICE is provided by
-                Kaito Ikeda at no cost and is intended for use as
-                is.
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>1. 学生証から収集するデータについて</strong></p>
+            <p class="text-xs">
+                デバイスで個人を識別する際に、学生証が必要となります。
+                これは、本人以外の操作・登録を防止するためであり、第三者による大量のデータの送信（攻撃）を防止するためでもあります。
+                学生証から取得するデータは、学籍番号8桁のみであり、その他のデータは一切取得しません。
             </p>
-            <br>
-            <p class="text-sm">
-                This page is used to inform visitors regarding my
-                policies with the collection, use, and disclosure of Personal
-                Information if anyone decided to use my Service.
-            </p>
-            <br>
-            <p class="text-sm">
-                If you choose to use my Service, then you agree to
-                the collection and use of information in relation to this
-                policy. The Personal Information that I collect is
-                used for providing and improving the Service. I will not use or share your information with
-                anyone except as described in this Privacy Policy.
-            </p>
-            <br>
-            <p class="text-sm">
-                The terms used in this Privacy Policy have the same meanings
-                as in our Terms and Conditions, which is accessible at
-                cacao unless otherwise defined in this Privacy Policy.
-            </p>
-            <br>
             <br>
 
-            <p class="mb-3 text-lg"><strong>Information Collection and Use</strong></p>
-            <p class="text-sm">
-                For a better experience, while using our Service, I
-                may require you to provide us with certain personally
-                identifiable information, including but not limited to Okayama University Student ID, body temperature,
-                and health status.
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>2. 入力いただく情報について</strong></p>
+            <p class="text-xs">
+                入室する前に、現在の体温と体調不良でないかの調査を行います。
+                これまでと同様、体温については学務部で管理するためデータを送信しますが、体調不良に関する調査は送信しません。
+                これらのデータの保存方法については後述します(5章)。
             </p>
-            <br>
-            <div><p class="text-sm">
-                    The app does use third party services that may collect
-                    information used to identify you.
-                </p>
-                <br>
-                <p class="text-sm">
-                    Link to privacy policy of third party service providers used
-                    by the app
-                </p>
-                <ul>
-                    <li class="text-sm">
-                        ・<a class="text-themeColor" href="https://www.google.com/policies/privacy/"
-                            target="_blank" rel="noopener noreferrer">Google Play Services</a>
-                    </li>
-                </ul>
-            </div>
-            <br>
             <br>
 
-            <p class="mb-3 text-lg"><strong>Log Data</strong></p>
-            <p class="text-sm">
-                I want to inform you that whenever you
-                use my Service, in a case of an error in the app
-                I collect data and information (through third party
-                products) on your phone called Log Data. This Log Data may
-                include information such as your device Internet Protocol
-                (“IP”) address, device name, operating system version, the
-                configuration of the app when utilizing my Service,
-                the time and date of your use of the Service, and other
-                statistics.
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>3. 送信するデータについて</strong></p>
+            <p class="text-xs">
+                以上より、サーバに送信される情報は、学籍番号と体温のみであり、SSLにより暗号化されてサーバに送信されます。
+                また送信可能な端末はストア証明書で署名されている端末に制限し、それ以外の手段を用いたリクエストは全て受け付けないようにしています。
             </p>
-            <br>
             <br>
 
-            <p class="mb-3 text-lg"><strong>Cookies</strong></p>
-            <p class="text-sm">
-                Cookies are files with a small amount of data that are
-                commonly used as anonymous unique identifiers. These are sent
-                to your browser from the websites that you visit and are
-                stored on your device's internal memory.
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>4. データベース上での保存方法について</strong></p>
+            <p class="text-xs">
+                本サービスでは、氏名や所属団体等の情報は一切保管せず、学生それぞれの識別番号と各団体に紐づくランダムな識別子を用いて、データベースに保管しております。
+                そのため、第三者が個人や団体名などのプライバシーに関わる情報を、閲覧することができないようになっております。
+                また、収集したデータについては、3ヶ月保管のちデータベースから完全に削除されます。
             </p>
-            <br>
-            <p class="text-sm">
-                This Service does not use these “cookies” explicitly. However,
-                the app may use third party code and libraries that use
-                “cookies” to collect information and improve their services.
-                You have the option to either accept or refuse these cookies
-                and know when a cookie is being sent to your device. If you
-                choose to refuse our cookies, you may not be able to use some
-                portions of this Service.
-            </p>
-            <br>
             <br>
 
-            <p class="mb-3 text-lg"><strong>Service Providers</strong></p>
-            <p class="text-sm">
-                I may employ third-party companies and
-                individuals due to the following reasons:
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>5. アカウントの悪用対策について</strong></p>
+            <p class="text-xs">
+                セキュリティの観点から、各団体に交付してあるアカウントからは一切のデータの閲覧ができない権限に設定されており、アカウントの悪用の対策をしております。
+                また、端末からの不審な動作やQRコードの流出などが発生した場合は、こちらからすぐにアカウントの無効化を行えるようになっております。
+                QRコードが紛失・流出した場合は早急にご連絡ください。
             </p>
-            <br>
-            <ul class="text-sm">
-                <li>・To facilitate our Service;</li>
-                <li>・To provide the Service on our behalf;</li>
-                <li>・To perform Service-related services; or</li>
-                <li>・To assist us in analyzing how our Service is used.</li>
-            </ul>
-            <br>
-            <p class="text-sm">
-                I want to inform users of this Service
-                that these third parties have access to your Personal
-                Information. The reason is to perform the tasks assigned to
-                them on our behalf. However, they are obligated not to
-                disclose or use the information for any other purpose.
-            </p>
-            <br>
             <br>
 
-            <p class="mb-3 text-lg"><strong>Security</strong></p>
-            <p class="text-sm">
-                I value your trust in providing us your
-                Personal Information, thus we are striving to use commercially
-                acceptable means of protecting it. But remember that no method
-                of transmission over the internet, or method of electronic
-                storage is 100% secure and reliable, and I cannot
-                guarantee its absolute security.
+            <p class="mb-3 text-md text-gray-800 tracking-wide"><strong>6. その他</strong></p>
+            <p class="text-xs">
+                上記以外に少しでも疑問に思われることや心配な点がございましたら、<a class="text-themeColor" target="_blank" rel="noopener noreferrer"
+                                                   href="https://docs.google.com/forms/d/e/1FAIpQLSdS47woHtsbvv6xqjsxJYiej1RjcSDwG9FBWj2ZpEfaQFeD9w/viewform?usp=sf_link">こちらのフォーム</a> よりお気軽にご連絡ください。
             </p>
-            <br>
-            <br>
-
-            <p class="mb-3 text-lg"><strong>Links to Other Sites</strong></p>
-            <p class="text-sm">
-                This Service may contain links to other sites. If you click on
-                a third-party link, you will be directed to that site. Note
-                that these external sites are not operated by me.
-                Therefore, I strongly advise you to review the
-                Privacy Policy of these websites. I have
-                no control over and assume no responsibility for the content,
-                privacy policies, or practices of any third-party sites or
-                services.
-            </p>
-            <br>
-            <br>
-
-            <p class="mb-3 text-lg"><strong>Children’s Privacy</strong></p>
-            <p class="text-sm">
-                These Services do not address anyone under the age of 13.
-                I do not knowingly collect personally
-                identifiable information from children under 13 years of age. In the case
-                I discover that a child under 13 has provided
-                me with personal information, I immediately
-                delete this from our servers. If you are a parent or guardian
-                and you are aware that your child has provided us with
-                personal information, please contact me so that
-                I will be able to do necessary actions.
-            </p>
-            <br>
-            <br>
-
-            <p class="mb-3 text-lg"><strong>Changes to This Privacy Policy</strong></p>
-            <p class="text-sm">
-                I may update our Privacy Policy from
-                time to time. Thus, you are advised to review this page
-                periodically for any changes. I will
-                notify you of any changes by posting the new Privacy Policy on
-                this page.
-            </p>
-            <br>
-            <p class="text-sm">This policy is effective as of 2021-07-12</p>
-            <br>
-            <br>
-
-            <p class="mb-3 text-lg"><strong>Contact Us</strong></p>
-            <p class="text-sm">
-                If you have any questions or suggestions about my
-                Privacy Policy, do not hesitate to contact me at developer.ikep@gmail.com.
-            </p>
-            <br>
-            <p class="text-sm">This privacy policy page was created at
-                <a class="text-themeColor"
-                   href="https://privacypolicytemplate.net"
-                   target="_blank"
-                   rel="noopener noreferrer">privacypolicytemplate.net </a>
-                and modified/generated by
-                <a class="text-themeColor"
-                   href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank"
-                   rel="noopener noreferrer">App Privacy Policy Generator</a></p>
         </div>
     </div>
 @endsection
